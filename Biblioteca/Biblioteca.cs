6d2a77faf54f0a7d51bb06688ad9497d0422e39f
martinadigirolamo <martina.digirolamo.stud@ispascalcomandini.it>
2021-11-12 09:08:54 +0100

    Implementazione classe Biblioteca

--- Biblioteca/Biblioteca.cs
+++ Biblioteca/Biblioteca.cs
@@ -8,5 +8,28 @@ namespace Biblioteca
 {
     class Biblioteca
     {
+        protected string _nome;
+        protected string _indirizzo;
+        protected int _orarioApertura;
+        protected int _orarioChiusura;
+        protected List<string> _elencoLibri;
+
+        public string Nome {set; get; }
+        public string Indirizzo { set; get; }
+        public int OrarioApertura { set; get; }
+        public int OrarioChiusura { set; get; }
+        public List<string> ElencoLibri { set; get; }
+
+
+        public Biblioteca(string nome, string indirizzo, int orarioApertura, int orarioChiusura, List<string> elencoLibri)
+        {
+            Nome = nome;
+            Indirizzo = indirizzo;
+            OrarioApertura = orarioApertura;
+            OrarioChiusura = orarioChiusura;
+            ElencoLibri = elencoLibri;
+        }
+
+
     }
 }
