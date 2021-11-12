208fe795b1d034319faa5c3a70c8dcbc721543cf
martinadigirolamo <martina.digirolamo.stud@ispascalcomandini.it>
2021-11-12 08:52:48 +0100

    Implementata classe Libro

--- Biblioteca/Libro.cs
+++ Biblioteca/Libro.cs
@@ -8,5 +8,65 @@ namespace Biblioteca
 {
     class Libro
     {
+        protected string _autore;
+        protected string _titolo;
+        protected int _annoPubblicazione;
+        protected string _editore;
+        protected int _numeroPagine;
+
+        public string Autore
+        {
+            set; get;
+        }
+        public string Titolo
+        {
+            set; get;
+        }
+        public string Editore
+        {
+            set; get;
+        }
+        public int AnnoPubblicazione
+        {
+            set; get;
+        }
+        public int NumeroPagine
+        {
+            set; get;
+        }
+
+
+        public Libro(string autore, string titolo, int annoPubblicazione, string editore, int numeroPagine)
+        {
+            Autore = autore;
+            Titolo = titolo;
+            AnnoPubblicazione = annoPubblicazione;
+            Editore = editore;
+            NumeroPagine = numeroPagine;
+        }
+
+        public override string ToString()
+        {
+            return Autore+Titolo+AnnoPubblicazione+Editore+NumeroPagine.ToString();
+        }
+
+        public int ReadingTime()
+        {
+            int tempolettura = 0;
+
+            if(NumeroPagine<=100)
+            {
+                tempolettura = 1;
+            }
+            else if (NumeroPagine>100 && NumeroPagine<=200)
+            {
+                tempolettura = 2;
+            }
+            else
+            {
+                tempolettura = 3;
+            }
+            return tempolettura;
+        }
     }
 }
